3c25f93b28295f2e484f2b7ccba451b822a773e6
David Henry <dw_henry@yahoo.com.au>
2014-11-02 17:31:08 +0000

    Add base sinatra implementation based on (https://github.com/zapnap/sinatra-template)

--- Gemfile
+++ Gemfile
@@ -1,10 +1,11 @@
 source 'https://rubygems.org'
 
 gem 'octokit'
+gem 'sinatra'
+gem 'haml'
 
 group :test do
   gem 'rspec'
-
   gem 'pry'
   gem 'pry-nav'
   gem 'webmock'
---
+++ application.rb
@@ -0,0 +1,26 @@
+$LOAD_PATH << './lib'
+
+require 'rubygems'
+require 'bundler/setup'
+require 'sinatra'
+require 'haml'
+require 'github_account'
+
+configure do
+  set :views, "#{File.dirname(__FILE__)}/views"
+end
+
+error do
+  e = request.env['sinatra.error']
+  Kernel.puts e.backtrace.join("\n")
+  'Application error'
+end
+
+helpers do
+  # add your helpers here
+end
+
+# root page
+get '/' do
+  haml :root
+end
