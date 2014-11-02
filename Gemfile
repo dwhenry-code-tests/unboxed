4e69aa24584074b8db4cf74d1f29f976355cba1c
David Henry <dw_henry@yahoo.com.au>
2014-11-02 02:25:16 +0000

    Add pry for debugging

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ gem 'octokit'
 group :test do
   gem 'rspec'
 
+  gem 'pry'
+  gem 'pry-nav'
   gem 'webmock'
   gem 'vcr'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 Dir.glob(::File.expand_path('../support/*.rb', __FILE__)).each { |f| require_relative f }
 
 RSpec.configure do |config|
