94eb49bf048126abf2ce0aadad9e8fa5327dd1c8
David Henry <dw_henry@yahoo.com.au>
2014-11-02 03:06:01 +0000

    Add some conditional logic to sorting when multiple winners (ie. languages with same number of repos)

--- lib/favourite_language_selector/most_popular_language.rb
+++ lib/favourite_language_selector/most_popular_language.rb
@@ -5,12 +5,26 @@ module FavouriteLanguageSelector
     end
 
     def get
+      current_time = Time.now
       language, _ = @repositories
       .group_by(&:language)
-      .sort_by{|_, repos| repos.count}
+      .sort_by do |_, repos|
+        [
+          repos.count,
+          -total_time_since_update(repos, current_time) # sum of time since last updated for all repos (desc)
+        ]
+      end
       .last
 
       language
+    end
+
+    private
+
+    def total_time_since_update(repos, current_time)
+      repos.inject(0.0) do |sum, repo|
+        sum + (current_time - repo.updated_at)
+      end
     end
   end
 end
--- spec/favourite_language_selector/most_popular_language_spec.rb
+++ spec/favourite_language_selector/most_popular_language_spec.rb
@@ -1,12 +1,13 @@
 require 'spec_helper'
 require 'favourite_language_selector/most_popular_language'
+require 'date'
 
 describe FavouriteLanguageSelector::MostPopularLanguage do
-  context 'when more of one language than any other' do
-    let(:with_ruby_language) { OpenStruct.new(language: 'Ruby') }
-    let(:with_java_language) { OpenStruct.new(language: 'Java') }
-    let(:with_c_language) { OpenStruct.new(language: 'C') }
+  let(:with_ruby_language) { OpenStruct.new(language: 'Ruby', updated_at: Time.now - 100) }
+  let(:with_java_language) { OpenStruct.new(language: 'Java', updated_at: Time.now - 50) }
+  let(:with_c_language) { OpenStruct.new(language: 'C', updated_at: Time.now - 75) }
 
+  context 'when more of one language than any other' do
     subject do
       described_class.new([
         with_java_language,
@@ -19,8 +20,25 @@ describe FavouriteLanguageSelector::MostPopularLanguage do
       ])
     end
 
-    it 'selects the language with the most occurances' do
+    it 'selects the language with the most occurrences' do
       expect(subject.get).to eq('Ruby')
+    end
+  end
+
+  context 'when two language occur the same number of times in the list' do
+    subject do
+      described_class.new([
+        with_java_language,
+        with_ruby_language,
+        with_java_language,
+        with_ruby_language,
+        with_java_language,
+        with_ruby_language,
+      ])
+    end
+
+    it 'selects the language with the minimum time since last updated' do
+      expect(subject.get).to eq('Java')
     end
   end
 
